fe1c2ced8c3952064e8f789e707ce9f27383a84d
Jason Jeffries <fuzablesoftware@outlook.com>
2015-12-01 13:13:48 -0500

    add basics of podcast and episode classes; remove naming option; reference models and core from CLI

---
+++ Podcast.Models/Podcast.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.AccessControl;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fuzable.Podcast.Models
+{
+    class Podcast
+    {
+        public string Name { get; set; }
+        public string Url { get; set; }
+        public int EpisodesToKeep { get; set; }
+        public List<Episode> EpisodesToDownload { get; set; }
+        public List<Episode> EpisodesToDelete { get; set; }
+    }
+}
