00bc45fbf9dc9b5e8f6486f1c5f802641806fd60
Jason Jeffries <fuzablesoftware@outlook.com>
2016-03-04 16:35:43 -0500

    move getfoldersfor and group size check to group class

--- Podcast.Models/Group.cs
+++ Podcast.Models/Group.cs
@@ -1,4 +1,9 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Xml.Linq;
+using Fuzable.Podcast.Entities.Properties;
 
 namespace Fuzable.Podcast.Entities
 {
@@ -15,5 +20,53 @@ namespace Fuzable.Podcast.Entities
         /// List of podcasts to copy in this group
         /// </summary>
         public List<Podcast> Podcasts { get; set; }
+
+        internal static string[] GetFolders(string group, string downloadFolder)
+        {
+            if (group == null)
+            {
+                //get folders in download folder
+                return Directory.GetDirectories(downloadFolder);
+            }
+            else
+            {
+                //get for group
+                //read podcasts.xml file and extract groups podcasts from
+                var folders = new List<Podcast>();
+
+                try
+                {
+                    var settingsDoc = XDocument.Load($@"{Environment.CurrentDirectory}\{"Podcasts.xml"}");
+                    var items = from item in settingsDoc.Root?.Elements("Groups").Descendants("Group").Descendants("Podcasts").Descendants("Podcast")
+                                select new
+                                {
+                                    Name = item.Element("Name")?.Value
+                                };
+                    folders.AddRange(items.Select(item => new Podcast(item.Name)));
+                }
+                catch (Exception ex)
+                {
+                    var error = new ApplicationException("Error retrieving groups", ex);
+                    throw error;
+                }
+                //filter
+                var includedFolders = folders.Select(s => Path.Combine(downloadFolder, s.Name)).ToArray();
+                return includedFolders;
+            }
+        }
+
+        internal static bool ExceedsMaximumSize(string group, string[] downloadFolders)
+        {
+            var max = Settings.Default.MaximumGroupSize;
+            if (group == null)
+            {
+                //all files and folders in download folder
+                var info = new DirectoryInfo(downloadFolders[0]);
+                var size = info.EnumerateFiles("*", SearchOption.AllDirectories).Sum(fi => fi.Length);
+                return size > max;
+            }
+            var total = downloadFolders.Select(downloadFolder => new DirectoryInfo(downloadFolder)).Select(info => info.EnumerateFiles("*", SearchOption.AllDirectories).Sum(fi => fi.Length)).Sum();
+            return total > max;
+        }
     }
 }
--- Podcast.Models/Subscription.cs
+++ Podcast.Models/Subscription.cs
@@ -225,12 +225,12 @@ namespace Fuzable.Podcast.Entities
                 throw new FileNotFoundException("Specified destination folder does not exist");
             }
 
-            var folders = GetFoldersInGroup(group, downloadFolder);
+            var folders = Group.GetFolders(group, downloadFolder);
             
             //does group file size exceed max?
-            if (MaximumGroupSizeExceeded(group, folders))
+            if (Group.ExceedsMaximumSize(group, folders))
             {
-                throw new ArgumentException("specified group exceeds maximum copy size");
+                throw new ArgumentException("specified group exceeds maximum size");
             }
 
             //copy files in each folder to destination
@@ -251,13 +251,8 @@ namespace Fuzable.Podcast.Entities
                 {
                     Podcasts = GetPodcasts();
                 }
+
                 var podcast = (Podcasts.Find(p => p.Name == podcastName));
-                if (podcast?.Order == Podcast.EpisodeOrder.Chronological)
-                {
-                    //order should be reverse of how they were downloaded
-                    //Array.Reverse(files); 
-                    //Array.Reverse reorders the array but USB clients (home receiver and car) seem to be listing in alpha (despite car docs indicating write order)
-                }
 
                 //count files as they are processed
                 var fileIndex = 0;
@@ -265,6 +260,7 @@ namespace Fuzable.Podcast.Entities
                 {
                     //get source path
                     var filename = Path.GetFileName(file) ?? "IDK";
+
                     //get destination path
                     var podcastFolder = Path.Combine(destinationFolder, podcastName);
                     //destination filename is used by player to organize
@@ -322,54 +318,6 @@ namespace Fuzable.Podcast.Entities
             var end = DateTime.Now;
             var lapsed = end - start;
             OnSubscriptionCopied(index, lapsed);
-        }
-
-        private static string[] GetFoldersInGroup(string group, string downloadFolder)
-        {
-            if (group == null)
-            {
-                //get folders in download folder
-                return Directory.GetDirectories(downloadFolder);
-            }
-            else
-            {
-                //get for group
-                //read podcasts.xml file and extract groups podcasts from
-                var folders = new List<Podcast>();
-
-                try
-                {
-                    var settingsDoc = XDocument.Load($@"{Environment.CurrentDirectory}\{"Podcasts.xml"}");
-                    var items = from item in settingsDoc.Root?.Elements("Groups").Descendants("Group").Descendants("Podcasts").Descendants("Podcast")
-                                select new
-                                {
-                                    Name = item.Element("Name")?.Value
-                                };
-                    folders.AddRange(items.Select(item => new Podcast(item.Name)));
-                }
-                catch (Exception ex)
-                {
-                    var error = new ApplicationException("Error retrieving groups", ex);
-                    throw error;
-                }
-                //filter
-                var includedFolders = folders.Select(s => Path.Combine(downloadFolder, s.Name)).ToArray();
-                return includedFolders;
-            }
-        }
-
-        private static bool MaximumGroupSizeExceeded(string group, string[] downloadFolders)
-        {
-            var max = Settings.Default.MaximumGroupSize;
-            if (group == null)
-            {
-                //all files and folders in download folder
-                var info = new DirectoryInfo(downloadFolders[0]);
-                var size = info.EnumerateFiles("*", SearchOption.AllDirectories).Sum(fi => fi.Length);
-                return size > max;
-            }
-            var total = downloadFolders.Select(downloadFolder => new DirectoryInfo(downloadFolder)).Select(info => info.EnumerateFiles("*", SearchOption.AllDirectories).Sum(fi => fi.Length)).Sum();
-            return total > max;
         }
 
         #region Event Handlers
